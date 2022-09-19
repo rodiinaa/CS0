d769b5146446ca2d32c422c53aac2656f4d8dad2
Rodina Karki <karkirodi@gmail.com>
2022-09-19 17:00:21 -0600

    description

---
+++ Assignments/stdio/stdio.py
@@ -0,0 +1,9 @@
+# Rodina Karki
+# Class: CS0
+# Date: 09/19/2022
+# Description: Homework Assignment 
+# Write programmer information and briefly describe what the program is about at the top of the program as comments (10 points)
+#Prompt user/player to enter their name; store the name into a variable
+#Greet the player using their name (20 points)
+#Using print function, draw several stages of the game. The partial output of the program, e.g., should look like the following. The blue text is user input. (60 points):
+#Update README.md file with the status and self grading as shown below (10 points)
